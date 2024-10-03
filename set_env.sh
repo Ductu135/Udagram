ed8562cc51f2fb93093d782ef98f479ef130c832
Tu DucBui <ductuhp135@gmail.com>
2024-10-04 01:50:01 +0700

    update

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=12345678
-export POSTGRES_HOST=database-udagram-1.cyz9ouhfp3pq.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-udagram.cyz9ouhfp3pq.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::myudagrambucket
 export AWS_REGION=us-east-1
